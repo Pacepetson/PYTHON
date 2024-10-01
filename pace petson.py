c63771dd52757b9af3ef4c7439c560a420e8044c
Pacepetson <pacepetson2028@cs.sjcetpalai.ac.in>
2024-10-01 15:22:56 +0530

    Add files via upload
    document header added

--- pace petson.py
+++ pace petson.py
@@ -1,4 +1,7 @@
-
+'''''
+Python Program to get the student details 
+Author: Pace Petson
+#Date: 01-10-2024''''
 name=input("Enter your name:")
 print(name)
 roll_number = int(input("Enter the Roll No:"))
