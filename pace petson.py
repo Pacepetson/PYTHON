4b822c9c24701d94d20dfe20811f515f27a0c2a2
Pacepetson <pacepetson2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:58 +0530

    Add files via upload
    My first python programme

---
+++ pace petson.py
@@ -0,0 +1,9 @@
+
+name=input("Enter your name:")
+print(name)
+roll_number = int(input("Enter the Roll No:"))
+print("Name of the student:",name)
+cgpa=float(input("Enter the CGPA:"))
+percentage_mark = cgpa*10
+
+
