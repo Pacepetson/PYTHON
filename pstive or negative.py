6a7ad258eca07b9540b3e65029e7a64fb1bb3ce3
Pacepetson <pacepetson2028@cs.sjcetpalai.ac.in>
2024-10-15 16:00:17 +0530

    Add files via upload

---
+++ pstive or negative.py
@@ -0,0 +1,11 @@
+'''
+python program to check whether the given number is postive or not
+'''
+
+number=int(input("enter a number:"))
+if number>0:
+    print("the given number:", number,"is postive")
+else:
+    print("the given number:",number,"is negative")
+
+
